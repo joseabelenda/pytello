d82ea1d85d0b47d59c88b5c67235fb50652e1a3a
José Abelenda <jose.abelenda@liferay.com>
2024-06-04 21:41:11 -0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+#!python3
+import api_manager
+import logging
+import sys
+from drone_manager import DroneManager
+
+logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+logger = logging.getLogger(__name__)
+
+# try:
+#     drone_manager = DroneManager()
+# except Exception as exception:
+#     logger.error(exception)
+
+# drone_manager.takeoff()
+# time.sleep(5)
+
+# drone_manager.up()
+# time.sleep(5)
+
+# drone_manager.right()
+# time.sleep(5)
+
+# drone_manager.left()
+# time.sleep(5)
+
+# drone_manager.land()
+# drone_manager.stop()
+
+api_manager.run()
